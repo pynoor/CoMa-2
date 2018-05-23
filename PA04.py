cbb70e0d941c2b184adb3bc4c3c323c2ff0be6e4
pynoor <32771762+pynoor@users.noreply.github.com>
2018-05-23 12:39:43 +0200

    Adjust UnionFind methods according to the instruction sheet

--- PA04.py
+++ PA04.py
@@ -3,6 +3,7 @@ def kruskal(IncidenceList):
     edges = sort_edges(edges)
     forest = UnionFind()
     forest.subsets = [0]*len(IncidenceList)
+    forest.p = [0]*len(IncidenceList)
     for vertice in range(len(IncidenceList)):
         forest.make_set(vertice)
     min_tree = []
@@ -39,12 +40,30 @@ class UnionFind:
 
     def make_set(self, vertice):
         self.subsets[vertice] = vertice
+        self.p.append(-1)
+        #self.p[vertice] = -len(self.subsets[vertice])
 
     def find_set(self, vertice):
-        return self.subsets[vertice]
+        for tree in self.subsets:
+            if vertice in tree:
+                for other_vertice in tree:
+                    if tree.index(other_vertice) < tree.index(vertice):
+                        self.p[other_vertice] = "r"
+        return "r"
+        #return self.subsets[vertice]
+
+
+    def get_root(self, subtree):
+        pass
 
     def union(self, start,target):
-        store = self.subsets[start]
-        for vertice in range(len(self.subsets)):
-            if self.subsets[vertice] == store:
-                self.subsets[vertice] = self.subsets[target]
+        found_start_subtree, found_target_subtree = find_set(start), find_set(target)
+        if found_start_subtree <= found_target_subtree:
+            self.p(get_root(found_start_subtree)) = self.p(get_root(found_target_subtree))
+            self.p(get_root(found_target_subtree)) = "size of combined tree"
+
+
+#        store = self.subsets[start]
+#        for vertice in range(len(self.subsets)):
+#            if self.subsets[vertice] == store:
+#                self.subsets[vertice] = self.subsets[target]
